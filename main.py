c6c4f7c46edaffaee3baeb7234af2eebffdbebe7
vandech <ndechi2009@gmail.com>
2025-01-23 23:48:48 -0600

    Modifying the random forest script and adding the train_random_forest steps

--- main.py
+++ main.py
@@ -115,8 +115,21 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
+            # Training the random_forest
 
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config['modeling']['stratify_by'],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config['modeling']['max_tfidf_features'],
+                    "output_artifact": config['modeling']['export_artifact']
+                },
+            )
 
         if "test_regression_model" in active_steps:
 
