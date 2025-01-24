bef294289a00c7433deb0a3d19e888c391457cb7
vandech <ndechi2009@gmail.com>
2025-01-23 22:36:38 -0600

    Adding data_check steps

--- main.py
+++ main.py
@@ -54,18 +54,41 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            # basic preprocessing
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                entry_point="main",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_sample",
+                    "output_description": "Data with outliers and null values removed",
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_check" in active_steps:
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
             # Implement here #
             ##################
+            # splitting into train and test
             pass
 
         if "train_random_forest" in active_steps:
@@ -91,7 +114,6 @@ def go(config: DictConfig):
             ##################
 
             pass
-
 
 if __name__ == "__main__":
     go()
